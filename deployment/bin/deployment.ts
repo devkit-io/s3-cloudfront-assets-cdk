91bf4b122c20d6721e2826dfef7e71ae4b0dd69a
User <colemurray.cs@gmail.com>
2023-10-23 22:23:24 -0700

    Add basic type for configuration type error

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
 import * as cdk from 'aws-cdk-lib';
-import {configuration as config} from "./config"
+import {configuration} from "./config"
 import {AssetCloudfrontDeploymentStack} from "../lib/stack";
 
 const app = new cdk.App();
+
+// due to optionals being in the dynamically generated config, this is to avoid a typescript error
+let config: Record<string,any> = configuration;
 new AssetCloudfrontDeploymentStack(app, `${config.repoName}-AssetStack`, {
     certificateArn: config.certificateArn,
     domainNames: config.domainName ? [config.domainName] : undefined,
